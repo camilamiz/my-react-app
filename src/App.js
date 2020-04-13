eef1134deedb5175c772eb949f81be5cdf5c016a
Camila Mizumoto <camila.yuri.m@gmail.com>
2020-04-13 18:49:59 -0300

    Starting the app and first exercises

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Camila', age: 37 },
+      {name: 'Marcel', age: 34 },
+      {name: 'Juju', age: 6 }
+    ],
+    otherState: 'some other value'
+  }
+
+  // Handler pra indicar que é um event handler
+  switchNameHandler = () => {
+    // console.log('was clicked!');
+    // DON'T DO THIS >> this.state.persons[0].name = 'Logan';
+    this.setState( {
+      persons: [
+        {name: 'Camila Yuri', age: 37 },
+        {name: 'Marcel', age: 34 },
+        {name: 'Juju', age: 5 }
+      ]
+    } )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Coding</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Surfing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+      </div>
+    );
+  }
 }
 
 export default App;
