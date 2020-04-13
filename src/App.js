cfa6fd53c9c4776e2c456a06cdfdfef05d475081
Camila Mizumoto <camila.yuri.m@gmail.com>
2020-04-13 20:43:41 -0300

    Revert "React hooks example"
    This reverts commit a84e64f5e9a75f315d1253bbb645eb1bb9485f88.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
-import React, { useState } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const [ personsState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
     persons: [
       {name: 'Finn', age: 12 },
       {name: 'Jake', age: 28 },
       {name: 'Princess Bubblegum', age: 6 }
     ],
     otherState: 'some other value'
-  });
-
-  const [ otherState, setOtherState ] = useState('someother value');
-
-  console.log(personsState, otherState)
+  }
 
   // Handler pra indicar que é um event handler
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     // console.log('was clicked!');
     // DON'T DO THIS >> this.state.persons[0].name = 'Logan';
-    setPersonsState( {
+    this.setState( {
       persons: [
         {name: 'Finn', age: 17 },
         {name: 'Jake', age: 34 },
         {name: 'Marceline', age: 1000 }
       ]
-    } );
-  };
+    } )
+  }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies: Adventures</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Parties</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Coding</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Surfing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+      </div>
+    );
+  }
+}
 
 export default App;
